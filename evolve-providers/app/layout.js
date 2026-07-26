a0004f1bc5e466d495720b377908d62193234866
Steven-Lannon <78940241+Steven-Lannon@users.noreply.github.com>
2026-07-26 15:19:49 -0400

    Update layout.js

--- evolve-providers/app/layout.js
+++ evolve-providers/app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import SiteHeader from "./components/SiteHeader";
 
 export const metadata = {
   metadataBase: new URL("https://evolvepsychiatry.com"),
@@ -7,7 +8,58 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://static1.squarespace.com/static/versioned-site-css/6525fe2f00c9de2ec400ea4f/1158/5c5a519771c10ba3470d8101/6525fe2f00c9de2ec400ea58/1810/site.css?nocustom=true"
+        />
+        <link
+          rel="stylesheet"
+          href="https://static1.squarespace.com/static/vta/5c5a519771c10ba3470d8101/versioned-assets/1784658820989-T86EF34M4QVPBBCSSFUN/static.css"
+        />
+        <link
+          rel="stylesheet"
+          href="https://static1.squarespace.com/static/custom-css/6525fe2f00c9de2ec400ea4f/6525fe2f00c9de2ec400ea58/741/custom.css"
+        />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+        />
+        <style
+          dangerouslySetInnerHTML={{
+            __html: `
+@font-face {
+  font-family: 'Poppins';
+  font-style: normal;
+  font-weight: 400;
+  font-display: swap;
+  src: url('https://file.squarespace-cdn.com/content/v2/namespaces/fonts/libraries/sqsp/assets/da0c3c0d-458f-46e2-9c64-bac004d4e8c1/latin.woff2') format('woff2');
+  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+}
+@font-face {
+  font-family: 'Poppins';
+  font-style: normal;
+  font-weight: 500;
+  font-display: swap;
+  src: url('https://file.squarespace-cdn.com/content/v2/namespaces/fonts/libraries/sqsp/assets/abb8d202-34bc-4174-a25f-c00d4154bb44/latin.woff2') format('woff2');
+  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+}
+@font-face {
+  font-family: 'Poppins';
+  font-style: normal;
+  font-weight: 700;
+  font-display: swap;
+  src: url('https://file.squarespace-cdn.com/content/v2/namespaces/fonts/libraries/sqsp/assets/95402d3d-cf4f-4ff3-b652-fa1bddbe79de/latin.woff2') format('woff2');
+  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+}
+`,
+          }}
+        />
+      </head>
+      <body>
+        <SiteHeader />
+        {children}
+      </body>
     </html>
   );
 }
