e6767994a2ec4daf2aff577768e0006f8656bfee
Steven-Lannon <78940241+Steven-Lannon@users.noreply.github.com>
2026-07-26 22:19:57 -0400

    Update ProviderDirectory.js

--- evolve-providers/app/components/ProviderDirectory.js
+++ evolve-providers/app/components/ProviderDirectory.js
@@ -470,7 +470,6 @@ export default function ProviderDirectory({ providers, typeLabel, heading, cross
           <line x1="12" y1="19" x2="12" y2="5" />
           <polyline points="5 12 12 5 19 12" />
         </svg>
-        Back to top
       </button>
     </div>
   );
